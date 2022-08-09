25afb0c984940915324aca456ea017a64d669cfe
Riponbarua <110828438+Riponbarua@users.noreply.github.com>
2022-08-09 19:49:44 +0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("My name is Ripon Barua");
+        System.out.println("I come from Bangladesh");
+        System.out.println("I am 25 years old");
+    }
+}
---
+++ variables.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello world!");
+        System.out.println("Hello java \n hello world");
+
+        System.out.println("**********");
+
+    // Print the patten
+    //*
+    //**
+    // ***
+    //****
+
+    System.out.println("*\n**\n***\n****");
+
+
+
+    // variable
+        //String name = " Ripon";
+        // int age = 48;
+        //double price = 25.25;
+        // int a= 25;
+       //int b= 10;
+
+        int a = 10;
+        int b= 20;
+        int addition = a+b;
+        int minus = a-b;
+        int multiplication=a*b;
+
+        System.out.println("**********");
+
+
+        System.out.println("Answer: "+addition);
+        System.out.println("Answer: "+minus);
+        System.out.println("Answer: "+multiplication);
+
+        System.out.println("**********");
+
+   
+
+
+
+}
+}
